eed7b61166aa298e663fd84f99cf44c4f17bb2c7
robvanbentem <gitbor87>
2013-01-30 16:36:55 +0100

    javascript for groceries

--- application/views/shoplist/form.blade.php
+++ application/views/shoplist/form.blade.php
@@ -1,6 +1,6 @@
-<form class="form form-inline" action="{{ URL::to_action('shoplist@add') }}">
-    {{ Form::span3_select('shop_id', $shared_shops); }}
-    {{ Form::span2_select('shop_id', $amounts, $default_amount); }}
-    {{ Form::span6_text('product') }}
+<form class="form form-inline form-add-product">
+    {{ Form::span3_select('shop_id', $shared_shops, null, array('id' => 'gl_shop_id')); }}
+    {{ Form::span2_select('amount', $amounts, $default_amount, array('id' => 'gl_amount')); }}
+    {{ Form::span6_text('product', null, array('id' => 'gl_product')) }}
     {{ Button::primary_submit('<span class="icon icon-white icon-shopping-cart"></span>') }}
 </form>
--- public/js/site.js
+++ public/js/site.js
@@ -1,3 +1,38 @@
 $(document).ready(function () {
-
+    var groceries = new Groceries();
+    new GroceryView({groceries: groceries});
 });
+
+var Groceries = function() {};
+
+Groceries.prototype.add = function(options){
+    $.ajax({
+        url: '/ajax/groceries/add',
+        type: 'POST',
+        dataType: 'json',
+        data: options.data,
+        success: options.success
+    });
+};
+
+var GroceryView = function(options) {
+    this.groceries = options.groceries;
+
+    var add = $.proxy(this.add, this);
+    $('.form-add-product').submit(add);
+};
+
+GroceryView.prototype.add = function(e) {
+    e.preventDefault();
+
+    this.groceries.add({
+        data: {
+            shop_id: $('#gl_shop_id').val(),
+            amount: $('#gl_amount').val(),
+            product: $('#gl_product').val()
+        },
+        success: function(data) {
+            console.log(data);
+        }
+    });
+};
